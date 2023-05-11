e8b0ff90890dab59595c5e70ee4098f95536d699
Steven Cucolo <8864661+scuc@users.noreply.github.com>
2023-05-11 14:34:37 -0400

    Updates all code and methods used in the script, integrates the argparser data

--- charchecker/__main__.py
+++ charchecker/__main__.py
@@ -4,27 +4,11 @@ import os
 from datetime import datetime
 from time import localtime, strftime
 
+import check_path
 import yaml
-
-
 from argparser import build_parser
 
-
 logger = logging.getLogger(__name__)
-
-
-def get_config():
-    """
-    Setup configuration and credentials
-    """
-
-    with open("charchecker/config.yaml", "rt") as f:
-        config = yaml.safe_load(f.read())
-
-    return config
-
-
-config = get_config()
 
 
 def set_logger():
@@ -57,7 +41,19 @@ def set_logger():
 
 def main():
     args = build_parser()
-    print(args)
+
+    args_msg = f"\n\
+     Aruguments used: \n\
+        Characters: {args.characters}\n\
+        Destination: {args.destination}\n\
+        Extra: {args.extra}\n\
+        Output: {args.output}\n\
+        Path: {args.path}\n\
+        Recursive: {args.recursive}\n\
+     "
+
+    logger.info(args_msg)
+
     date_start = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
 
     start_msg = f"\n\
@@ -68,15 +64,20 @@ def main():
 
     logger.info(start_msg)
 
-    date_end = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
+    exit_code = check_path.check_set_path(args)
 
-    complete_msg = f"\n\
-    ================================================================\n\
-                Illegal Character Check - Complete\n\
-                    {date_end}\n\
-    ================================================================\n\
-    "
-    logger.info(complete_msg)
+    if exit_code != 0:
+        logger.info("\nPath check did not complete sucessfully.\n")
+    else:
+        date_end = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
+
+        complete_msg = f"\n\
+        ================================================================\n\
+                    Illegal Character Check - Complete\n\
+                        {date_end}\n\
+        ================================================================\n\
+        "
+        logger.info(complete_msg)
     return
 
 
--- charchecker/check_path.py
+++ charchecker/check_path.py
@@ -1,24 +1,21 @@
 #! /usr/bin/env python3
 
-# import logging
+import logging
 import os
-from pathlib import Path
 import re
+import sys
 from collections import Counter
+from pathlib import Path
 from time import localtime, strftime
 
-# import config
-
-# logger = logging.getLogger(__name__)
-
-# config = config.get_config()
+logger = logging.getLogger(__name__)
 
 
-def check_pathname(path):
+def check_set_path(args):
     """
-    Check each path recursively, and elminiate any illegal characters.
+    Check each path and look for any illegal characters or whitespace.
     """
-
+    exitcode = 0
     date_start = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
 
     start_msg = f"\n\
@@ -26,165 +23,126 @@ def check_pathname(path):
                     START CHECK: {date_start}\n\
     ================================================================\n\
    "
-    write_path_to_txt(start_msg=start_msg)
-    print(start_msg)
+    write_to_file(start_msg=start_msg)
 
-    char_limit_count = 0
-    dir_count = 0
-    ds_count = 0
-    file_count = 0
-    illegal_char_list = []
-    illegal_dirname_total = 0
-    illegal_filename_total = 0
-    illegalchar_total = Counter({"illegal_char_count": 0, "whitespace_count": 0})
+    path_total = {
+        "char_limit_count": 0,
+        "dir_count": 0,
+        "ds_count": 0,
+        "file_count": 0,
+        "illegal_char_list": [],
+        "illegal_dirname_total": 0,
+        "illegal_filename_total": 0,
+    }
 
-    # try:
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in dirs:
-            path = Path(root, name)
-            dir_count += 1
-            illegalchar_count, illegal_chars = illegalchar_check(path)
-            # print(f"1 - DIR NAME: {pathname}")
-            illegalchar_total.update(illegalchar_count)
-            illegal_char_list += illegal_chars
+    illegal_total = Counter({"illegalchar_count": 0, "whitespace_count": 0})
 
-            if len(illegal_chars) != 0:
-                illegal_dirname_total += 1
-                # print(f"2 - ILLEGAL CHAR TOTAL: {illegalchar_total}")
-            else:
-                continue
+    # Note: have to treat recurive and non-recursive seperately because the
+    # recursive method uses topdown=false, it will not work for only top-level scan.
 
-        for name in files:
-            file_count += 1
-            path = Path(root, name)
-            if (
-                name.startswith(".DS_Store")
-                or name.startswith("._")
-                and os.stat(name).st_size < 5000
-            ):
-                ds_count += 1
-                continue
-            else:
-                whitespace_match = whitespace_check(path)
-                whitespace_count = len(whitespace_match)
-                if whitespace_count != 0:
-                    write_path_to_txt(
-                        illegal_path=path,
-                        whitespace_count=whitespace_count,
+    try:
+        if args.recursive is not True:
+            path_total, illegal_total = check_current_path(
+                args, args.path, path_total, illegal_total
+            )
+            illegal_total = whitespace_check(args.path, illegal_total)
+            summary = prepare_summary(path_total, illegal_total)
+            write_to_file(summary)
+            return exitcode
+
+        else:
+            for root, dirs, files in os.walk(args.path, topdown=False):
+                # Check all sub-dir in set path
+                for name in dirs:
+                    path = Path(root, name)
+                    path_total["dir_count"] += 1
+                    path_total, illegal_total = check_current_path(
+                        args, path, path_total, illegal_total
                     )
-                    illegalchar_total.update({"whitespace_count": whitespace_count})
 
-                illegal_char_count, illegal_chars = illegalchar_check(path)
+                # Check all files, in all sub-dir in set path
+                for name in files:
+                    path = Path(root, name)
+                    path_total["file_count"] += 1
+                    if (
+                        name.startswith(".DS_Store")
+                        or name.startswith("._")
+                        and os.stat(name).st_size < 5000
+                    ):
+                        path_total["ds_count"] += 1
+                        continue
+                    else:
+                        illegal_total = whitespace_check(path, illegal_total)
+                        path_total, illegal_total = illegalchar_check(
+                            args, path, path_total, illegal_total
+                        )
 
-                if len(illegal_chars) != 0:
-                    illegal_filename_total += 1
-                    illegalchar_total.update(illegal_char_count)
-                    # print(illegalchar_total)
-                    illegal_char_list += illegal_chars
-                    # print(illegal_char_list)
+                        if len(str(path)) > 255:
+                            illegal_path = path
+                            char_limit_msg = f"Too many characters for Windows path (>255): \n {illegal_path} "
+                            logger.info(char_limit_msg)
 
-                    write_path_to_txt(illegal_path=path, illegal_chars=illegal_chars)
+                            write_to_file(
+                                illegal_path=str(illegal_path),
+                                path_length=len(str(illegal_path)),
+                            )
+                            path_total["char_limit_count"] += 1
+                        else:
+                            pass
 
-                else:
-                    pass
+        summary = prepare_summary(args, path_total, illegal_total)
+        write_to_file(summary=summary)
+        return exitcode
 
-                if len(str(path)) > 255:
-                    illegal_path = path
-                    char_limit_msg = f"Too many characters for Windows path (>255): \n {illegal_path} "
-                    # logger.info(char_limit_msg)
-                    print(char_limit_msg)
-                    write_path_to_txt(
-                        illegal_path=str(illegal_path),
-                        path_length=len(str(illegal_path)),
-                    )
-                    char_limit_count += 1
-                else:
-                    # print(" === PASS ON PATH LEN")
-                    pass
-
-    # except Exception as e:
-    #     file_walk_msg = f"Exception on FILE Walk: \n {e}"
-    #     # logger.error(file_walk_msg)
-    #     print(file_walk_msg)
-
-    left_curly_brace_total = illegal_char_list.count("{")
-    right_curly_brace_total = illegal_char_list.count("}")
-    single_quote_total = illegal_char_list.count("'")
-    double_quote_total = illegal_char_list.count('"')
-
-    date_end = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
-    totals = f"\n========================== END CHECK: {date_end} ================================\n\
-            path checked = {path} \n\
-            {dir_count} sub-directories in path.\n\
-            {file_count-ds_count} files in path.\n\
-            {illegal_dirname_total} directory names with illegal characters.\n\
-            {illegal_filename_total} filenames with illegal characters.\n\
-            {char_limit_count} file paths that exceed the 255 Windows limit.\n\
-            {illegalchar_total['whitespace_count']} illegal whitespace characters found.\n\
-            {ds_count} .DS_Store files found in path.\n\
-            {len(illegal_char_list)} illegal characters in total: \n\
-            @ [{illegal_char_list.count('@')}] \n\
-            : [{illegal_char_list.count(':')}] \n\
-            * [{illegal_char_list.count('*')}] \n\
-            ? [{illegal_char_list.count('?')}] \n\
-            ! [{illegal_char_list.count('!')}] \n\
-            > [{illegal_char_list.count('>')}] \n\
-            < [{illegal_char_list.count('<')}] \n\
-            | [{illegal_char_list.count('|')}] \n\
-            & [{illegal_char_list.count('&')}] \n\
-            # [{illegal_char_list.count('#')}] \n\
-            % [{illegal_char_list.count('%')}] \n\
-            $ [{illegal_char_list.count('$')}] \n\
-            ~ [{illegal_char_list.count('~')}] \n\
-            + [{illegal_char_list.count('+')}] \n\
-            = [{illegal_char_list.count('+')}] \n\
-            {{ [{left_curly_brace_total}] \n\
-            }} [{right_curly_brace_total}] \n\
-            ' [{single_quote_total}] \n\
-            \" [{double_quote_total}] \n\
-    =================================================================================================\n\
-            "
-
-    write_path_to_txt(totals=totals)
-    print(illegal_char_list)
-    print("SCRIPT DONE")
-    return
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        excp_msg = f" Exception raised: {e}\n\
+                      TYPE: {exc_type},\n\
+                      FNAME: {fname},\n\
+                      LINENO: {exc_tb.tb_lineno}\n\
+                    "
+        logger.error(excp_msg)
+        exitcode = 1
+        return exitcode
 
 
-def illegalchar_check(path):
+def check_current_path(args, path, path_total, illegal_total):
+    """
+    Check the current path for whitespace and illegal characters.
+    """
+    try:
+        whitespace_match = whitespace_check(path, illegal_total)
+        whitespace_count = len(whitespace_match)
+        illegal_total.update({"whitespace_count": whitespace_count})
+
+        path_total, illegal_total = illegalchar_check(
+            args, path, path_total, illegal_total
+        )
+
+        return path_total, illegal_total
+
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        excp_msg = f" Exception raised: {e}\n\
+                      TYPE: {exc_type},\n\
+                      FNAME: {fname},\n\
+                      LINENO: {exc_tb.tb_lineno}\n\
+                    "
+        logger.error(excp_msg)
+
+
+def illegalchar_check(args, path, path_total, illegal_total):
     """
     Check a path name against a list of illegal characters, remove any found.
     """
-    illegal_char_count = 0
+    illegalchar_count = 0
     illegal_chars = []
 
-    illegalchars = [
-        "@",
-        ":",
-        "*",
-        "?",
-        "!",
-        '"',
-        "'",
-        "<",
-        ">",
-        "|",
-        "&",
-        "#",
-        "%",
-        "$",
-        "~",
-        "+",
-        "=",
-        "'",
-        '"',
-        "{",
-        "}",
-        "^",
-    ]
-    illegal_char_count = len([x for x in path.name if x in illegalchars])
-    illegal_char = [x for x in path.name if x in illegalchars]
-    print(f"3 - ILLEGAL CHAR: {illegal_char}")
+    illegalchar_list = [args.characters]
+
+    illegalchar_count = len([x for x in path.name if x in illegalchar_list])
 
     try:
         # regex to match on:
@@ -195,47 +153,139 @@ def illegalchar_check(path):
         pattern = re.compile(
             r"(\||\'|\"|\}|\{|\:|\=|\@|\*|\?|\!|\<|\>|\&|\#|\%|\$|\~|\+|\.$|^\.)"
         )
-        print(f"name: {path.name}")
+
         for match in re.findall(pattern, path.name):
-            illegal_char_count += 1 if match[0] != "" else 0
+            illegalchar_count += 1 if match[0] != "" else 0
             illegal_chars.append(match[0]) if match[0] != "" else None
 
+            if len(illegal_chars) != 0 and path.is_file():
+                path_total["illegal_filename_total"] += 1
+            elif len(illegal_chars) != 0 and path.is_dir():
+                path_total["illegal_dirname_total"] += 1
+
+            if len(illegal_chars) != 0:
+                path_total["illegal_char_list"] += illegal_chars
+                count = Counter(illegalchar_count=len(illegal_chars))
+                illegal_total.update(count)
+                write_to_file(illegal_path=path, illegal_chars=illegal_chars)
+            else:
+                continue
+
+        return path_total, illegal_total
+
     except Exception as e:
-        make_safe_msg = f"Exception raised on attempt find illegal characters: \n {e}"
-        # logger.error(make_safe_msg)
-        print(make_safe_msg)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        excp_msg = f" Exception raised: {e}\n\
+                      TYPE: {exc_type},\n\
+                      FNAME: {fname},\n\
+                      LINENO: {exc_tb.tb_lineno}\n\
+                    "
+        logger.error(excp_msg)
 
-    # if illegal_char_count > 0 or whitespace_count > 0:
-    #     illegal_path = os.path.join(root, name)
-    #     write_path_to_txt(illegal_chars=illegal_path)
-    # else:
-    #     pass
-
-    illegal_char_totals = {
-        "illegal_char_count": illegal_char_count,
-    }
-
-    return illegal_char_totals, illegal_chars
+    return illegalchar_count, illegal_chars
 
 
-def whitespace_check(path):
+def whitespace_check(path, illegal_total):
     pattern = re.compile(r"(^\s+|\s+$|\s+/|/\s+)")
     whitespace_match = re.findall(pattern, str(path))
-    print(whitespace_match, len(whitespace_match))
-    return whitespace_match
+    whitespace_count = len(whitespace_match)
+    if whitespace_count != 0:
+        write_to_file(
+            illegal_path=path,
+            whitespace_count=whitespace_count,
+        )
+        illegal_total.update({"whitespace_count": whitespace_count})
+    return illegal_total
 
 
-def write_path_to_txt(*args, **kwargs):
-    # req_zip_f = os.path.join(illegal_path[:28], "_Archive_REQ_ZIP")
-    # os.chdir(req_zip_f)
+def prepare_summary(args, path_total, illegal_total):
+    """
+    Prepare a summary of totals and pass it to write_to_file method.
+    """
+    left_curly_brace_total = path_total["illegal_char_list"].count("{")
+    right_curly_brace_total = path_total["illegal_char_list"].count("}")
+    single_quote_total = path_total["illegal_char_list"].count("'")
+    double_quote_total = path_total["illegal_char_list"].count('"')
+
+    date_end = str(strftime("%A, %d. %B %Y %I:%M%p", localtime()))
+    summary = f"\n========================== END CHECK: {date_end} ================================\n\
+            Recursive check: {args.recursive}\n\
+            path checked = {args.path} \n\
+            {path_total['dir_count']} sub-directories in path.\n\
+            {path_total['file_count']} files in path.\n\
+            {path_total['illegal_dirname_total']} directory names with illegal characters.\n\
+            {path_total['illegal_filename_total']} filenames with illegal characters.\n\
+            {path_total['char_limit_count']} file paths that exceed the 255 Windows limit.\n\
+            {illegal_total['whitespace_count']} illegal whitespace characters found.\n\
+            {path_total['ds_count']} .DS_Store files found in path.\n\
+            {len(path_total['illegal_char_list'])} illegal characters in total: \n\
+                @ [{path_total['illegal_char_list'].count('@')}] \n\
+                : [{path_total['illegal_char_list'].count(':')}] \n\
+                * [{path_total['illegal_char_list'].count('*')}] \n\
+                ? [{path_total['illegal_char_list'].count('?')}] \n\
+                ! [{path_total['illegal_char_list'].count('!')}] \n\
+                > [{path_total['illegal_char_list'].count('>')}] \n\
+                < [{path_total['illegal_char_list'].count('<')}] \n\
+                | [{path_total['illegal_char_list'].count('|')}] \n\
+                & [{path_total['illegal_char_list'].count('&')}] \n\
+                # [{path_total['illegal_char_list'].count('#')}] \n\
+                % [{path_total['illegal_char_list'].count('%')}] \n\
+                $ [{path_total['illegal_char_list'].count('$')}] \n\
+                ~ [{path_total['illegal_char_list'].count('~')}] \n\
+                + [{path_total['illegal_char_list'].count('+')}] \n\
+                = [{path_total['illegal_char_list'].count('+')}] \n\
+                {{ [{left_curly_brace_total}] \n\
+                }} [{right_curly_brace_total}] \n\
+                ' [{single_quote_total}] \n\
+                \" [{double_quote_total}] \n\
+    =================================================================================================\n\
+    "
+    logger.info(summary)
+    return summary
+
+
+def write_to_file(*args, **kwargs):
     file_date = str(strftime("%Y%m%d", localtime()))
+    loc = os.getcwd()
+    print(loc)
     with open(f"{file_date}_illegal_paths.txt", "a+") as f:
         for key, value in kwargs.items():
+            print(key, value)
             f.write(f"{key}: {value}\n")
-        f.write(f"\n")
+        f.write("\n")
         f.close()
     return
 
 
 if __name__ == "__main__":
-    check_pathname("/Users/cucos001/Desktop/test")
+    # one = [
+    #         "@",
+    #         ":",
+    #         "*",
+    #         "?",
+    #         "!",
+    #         '"',
+    #         "'",
+    #         "<",
+    #         ">",
+    #         "|",
+    #         "&",
+    #         "#",
+    #         "%",
+    #         "$",
+    #         "~",
+    #         "+",
+    #         "=",
+    #         "'",
+    #         '"',
+    #         "{",
+    #         "}",
+    #         "^",
+    #     ],
+    # two = "/Users/cucos001/GitHub/Illegal-Character-Check",
+    # three = None,
+    # four = ".txt",
+    # five = "/Users/cucos001/Desktop/test/",
+    # six = True,
+    check_set_path()
